093fd3b00512735d0f24f2a9b75331f2248d2a0c
Jacko Dirks <jacko.dirks@gmail.com>
2013-11-05 00:49:20 +0100

    Finished first version of code

--- software/VGA_test/main.c
+++ software/VGA_test/main.c
@@ -1,6 +1,11 @@
+//http://monash-psychophysics-test.googlecode.com/svn-history/r55/trunk/LowLevel/First_Proto/VGA_SD_Keyboard/Software/hello.c
+
 #include <stdio.h> //printf etc
 #include <unistd.h> //usleep
 #include "system.h" //declaration of available hardware
+#include "altera_up_avalon_video_pixel_buffer_dma.h" //For buffer swapping etc
+#include "altera_avalon_pio_regs.h"
+#include <sys/alt_irq.h>
 
 //Declarations of inputs and outputs
 volatile int * ledR = (int*) 0x00093050;
@@ -10,10 +15,12 @@ volatile char * character_buffer = (char *) 0x00090000;	// VGA character buffer
 volatile short * pixel_buffer_a = (short *) 0x00000000; //VGA pixel buffer A (SDRAM)
 volatile short * pixel_buffer_b = (short *) 0x00025800; //VGA pixel buffer B (SDRAM)
 volatile int edge_capture;
+alt_up_pixel_buffer_dma_dev *pixel_buffer_dev;
 
 #define RED 0xf800
 #define GREEN 0x7e0
 #define BLUE 0x1f
+#define USLEEP_TIME 100000
 
 void handle_button_interrupts(void* context){
 	volatile int* edge_capture_ptr = (volatile int*) context; //get the context and typecast it (?)
@@ -22,32 +29,6 @@ void handle_button_interrupts(void* context){
 	IORD_ALTERA_AVALON_PIO_EDGE_CAP(KEY_PIO_BASE); //Delay for a little bit to prevent imediate reïnterrupt
 	*ledG = *keys;
 	*ledR = *edge_capture_ptr; //Print the outputs to the LEDS to see what the program sees
-}
-
-int main(void){
-	//Setup the interrupts
-	IOWR_ALTERA_AVALON_PIO_IRQ_MASK(KEY_PIO_BASE, 0xf);
-	IOWR_ALTERA_AVALON_PIO_EDGE_CAP(KEY_PIO_BASE, 0x0);
-	alt_ic_isr_register(KEY_PIO_IRQ_INTERRUPT_CONTROLLER_ID,
-			KEY_PIO_IRQ,
-			handle_button_interrupts,
-			(void*) &edge_capture, 0x0);
-	alt_irq_enable(KEY_PIO_IRQ); //Not sure if necessary, starts the software interrupt (something with priority)
-	int colors[3] = {RED,GREEN,BLUE};
-	int colorIndicator = 0;
-	int singleBuffer = 1;
-	short* pixelBuffer = pixel_buffer_a; //The buffer for switching
-	while(1){
-		if ( colorIndicator > 2 ) colorIndicator = 0;
-		VGA_box (0, 0, 319, 239, 0, pixelBuffer); // clear screen
-		VGA_box (0 /*x1*/, 0 /*y1*/, 319 /*x2*/, 239 /*y2*/, colors[colorIndicator++], pixelBuffer);
-		if (!singleBuffer){
-			//switch buffer
-		}
-		//Double buffer code
-	}
-
-	return 0;
 }
 
 void VGA_box(int x1, int y1, int x2, int y2, short pixel_color, short* pixel_buffer)
@@ -64,4 +45,44 @@ void VGA_box(int x1, int y1, int x2, int y2, short pixel_color, short* pixel_buf
 			++col;
 		}
 	}
+}
+
+int main(void){
+	//Setup the interrupts
+	IOWR_ALTERA_AVALON_PIO_IRQ_MASK(KEY_PIO_BASE, 0xf);
+	IOWR_ALTERA_AVALON_PIO_EDGE_CAP(KEY_PIO_BASE, 0x0);
+	alt_ic_isr_register(KEY_PIO_IRQ_INTERRUPT_CONTROLLER_ID,
+			KEY_PIO_IRQ,
+			handle_button_interrupts,
+			(void*) &edge_capture, 0x0);
+	//alt_irq_enable(KEY_PIO_IRQ); //Not sure if necessary, starts the software interrupt (something with priority)
+	int colors[3] = {RED,GREEN,BLUE};
+	int colorIndicator = 0;
+	int singleBuffer = 1;
+	pixel_buffer_dev = alt_up_pixel_buffer_dma_open_dev("/dev/VGA_Pixel_Buffer");
+	volatile short* pixelBuffer = pixel_buffer_a; //The buffer for switching
+	while(1){
+		if ( colorIndicator > 2 ) colorIndicator = 0;
+		VGA_box (0, 0, 319, 239, 0, pixelBuffer); // clear screen
+		VGA_box (0 /*x1*/, 0 /*y1*/, 319 /*x2*/, 239 /*y2*/, colors[colorIndicator++], pixelBuffer);
+		if (!singleBuffer){
+			//switch buffer
+			if (!alt_up_pixel_buffer_dma_swap_buffers(pixel_buffer_dev)){
+				printf("Buffer swapping failed!\n");
+				return -1;
+			}
+			//Also swap internally
+			if (pixelBuffer == pixel_buffer_a){
+				pixelBuffer = pixel_buffer_b;
+			} else {
+				pixelBuffer = pixel_buffer_a;
+			}
+			//Wait for the swap to be done
+			while (alt_up_pixel_buffer_dma_check_swap_buffers_status(pixel_buffer_dev));
+		}
+		//Sleep, softly.
+		usleep(USLEEP_TIME);
+	}
+
+	return 0;
 }
--- software/VGA_test/obj/default/main.d
+++ software/VGA_test/obj/default/main.d
@@ -1,6 +1,40 @@
 obj/default/main.o: main.c ../NIOS2_VGA_bsp/system.h \
-  ../NIOS2_VGA_bsp/linker.h
+  ../NIOS2_VGA_bsp/linker.h \
+  ../NIOS2_VGA_bsp//drivers/inc/altera_up_avalon_video_pixel_buffer_dma.h \
+  ../NIOS2_VGA_bsp//HAL/inc/alt_types.h \
+  ../NIOS2_VGA_bsp//HAL/inc/sys/alt_dev.h ../NIOS2_VGA_bsp/system.h \
+  ../NIOS2_VGA_bsp//HAL/inc/sys/alt_llist.h \
+  ../NIOS2_VGA_bsp//HAL/inc/priv/alt_dev_llist.h \
+  ../NIOS2_VGA_bsp//HAL/inc/sys/alt_llist.h \
+  ../NIOS2_VGA_bsp//drivers/inc/altera_avalon_pio_regs.h \
+  ../NIOS2_VGA_bsp//HAL/inc/io.h ../NIOS2_VGA_bsp//HAL/inc/alt_types.h \
+  ../NIOS2_VGA_bsp//HAL/inc/sys/alt_irq.h \
+  ../NIOS2_VGA_bsp//HAL/inc/nios2.h
 
 ../NIOS2_VGA_bsp/system.h:
 
 ../NIOS2_VGA_bsp/linker.h:
+
+../NIOS2_VGA_bsp//drivers/inc/altera_up_avalon_video_pixel_buffer_dma.h:
+
+../NIOS2_VGA_bsp//HAL/inc/alt_types.h:
+
+../NIOS2_VGA_bsp//HAL/inc/sys/alt_dev.h:
+
+../NIOS2_VGA_bsp/system.h:
+
+../NIOS2_VGA_bsp//HAL/inc/sys/alt_llist.h:
+
+../NIOS2_VGA_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+../NIOS2_VGA_bsp//HAL/inc/sys/alt_llist.h:
+
+../NIOS2_VGA_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../NIOS2_VGA_bsp//HAL/inc/io.h:
+
+../NIOS2_VGA_bsp//HAL/inc/alt_types.h:
+
+../NIOS2_VGA_bsp//HAL/inc/sys/alt_irq.h:
+
+../NIOS2_VGA_bsp//HAL/inc/nios2.h:
