ed6905d8e658e17d10a5f46dbb373c98d32c2666
Jacko Dirks <jacko.dirks@gmail.com>
2013-11-20 22:45:47 +0100

    Updated main.c, added temp stuff

--- software/VGA_test/main.c
+++ software/VGA_test/main.c
@@ -3,67 +3,59 @@
 #include <stdio.h> //printf etc
 #include <unistd.h> //usleep
 #include "system.h" //declaration of available hardware
-#include "altera_up_avalon_video_pixel_buffer_dma.h" //For buffer swapping etc
+#include "altera_up_avalon_video_pixel_buffer_dma.h" //HAL for pixel buffer
+#include "altera_up_avalon_video_character_buffer_with_dma.h" //HAL for Character Buffer
 #include "altera_avalon_pio_regs.h"
 #include <sys/alt_irq.h>
 
-//Declarations of inputs and outputs
+//Hard Defines
+#define RED 0xf800 //1111000000000
+#define GREEN 0x7e0 //0000111110000
+#define BLUE 0x1f //0000000001111
+#define USLEEP_TIME 1000000
+
+//Declarations of positions of buffers and hardware components
 volatile int * ledR = (int*) 0x00093050;
 volatile int * ledG = (int*) 0x00093030;
 volatile int * keys = (int*) 0x00093020;
 volatile char * character_buffer = (char *) 0x00090000;	// VGA character buffer
-volatile int edge_capture;
-volatile void * end_front_buffer = 0x257FFF;
-volatile void * end_sram = 0x7FFFF;
+volatile float* fftA = (float *)0x4B000; //FFT A buffer
+
+
+//Global vars
+//Device names
+const char* pixelBufferName = "/dev/VGA_Pixel_Buffer";
+const char* charBufferName = "/dev/VGA_Character_buffer_avalon_char_control_slave";
+//Device pointers
 alt_up_pixel_buffer_dma_dev *pixel_buffer_dev;
+alt_up_char_buffer_dev *char_buffer_dev;
+//The values is value in Hz /1000
+unsigned short minval = 0;
+unsigned short maxVal = 100;
 
-#define RED 0xf800
-#define GREEN 0x7e0
-#define BLUE 0x1f
-#define USLEEP_TIME 1000000
+#include "FFT_temp.h"
 
-void handle_button_interrupts(void* context){
-	volatile int* edge_capture_ptr = (volatile int*) context; //get the context and typecast it (?)
-	*edge_capture_ptr = IORD_ALTERA_AVALON_PIO_EDGE_CAP(KEY_PIO_BASE); //Get the value
-	IOWR_ALTERA_AVALON_PIO_EDGE_CAP(KEY_PIO_BASE, 0); //Reset the interrupt
-	IORD_ALTERA_AVALON_PIO_EDGE_CAP(KEY_PIO_BASE); //Delay for a little bit to prevent imediate reïnterrupt
-	*ledG = *keys;
-	*ledR = *edge_capture_ptr; //Print the outputs to the LEDS to see what the program sees
-	//printf("Im HERE!\n");
+//This function does stuff that does not belong in the final version
+void temp(void){
+	//Write the const data to the mem
+	for (int i = 0; i < 1024; ++i){
+		for (int j = 0 ; j < 2; ++j){
+			(*fftA)[i][j] = tempFFT[i][j];
+		}
+	}
+}
+//This function initializes everything to get it prepared for work
+void init(void){
+	pixel_buffer_dev = alt_up_pixel_buffer_dma_open_dev(pixelBufferName); //Init HAL for pixelbuffer
+	char_buffer_dev = alt_up_char_buffer_open_dev(charBufferName); //Init HAL for char buffer
 }
 
 int main(void){
-	//Setup the interrupts
-	IOWR_ALTERA_AVALON_PIO_IRQ_MASK(KEY_PIO_BASE, 0xf);
-	IOWR_ALTERA_AVALON_PIO_EDGE_CAP(KEY_PIO_BASE, 0x0);
-	/*alt_ic_isr_register(KEY_PIO_IRQ_INTERRUPT_CONTROLLER_ID,
-			KEY_PIO_IRQ,
-			handle_button_interrupts,
-			(void*) &edge_capture, 0x0);*/
-	//alt_irq_enable(KEY_PIO_IRQ); //Not sure if necessary, starts the software interrupt (something with priority)
+	temp();
+	init();
 	int colors[3] = {RED,GREEN,BLUE};
 	int colorIndicator = 0;
 	int buffer = 1;
-	pixel_buffer_dev = alt_up_pixel_buffer_dma_open_dev("/dev/VGA_Pixel_Buffer");
-	/*while(1){
-		if ( colorIndicator > 2 ) colorIndicator = 0;
-		alt_up_pixel_buffer_dma_draw_box(pixel_buffer_dev, 0, 0, 319, 239, colors[colorIndicator++], buffer);
-		long i;
-		for (i = end_front_buffer; i < end_sram; ++i){
-			char * pixel = (char*) i;
-			*pixel = 0;
-		}
-		if (alt_up_pixel_buffer_dma_swap_buffers(pixel_buffer_dev)){
-			printf("Buffer swapping failed!\n");
-			return -1;
-		}
-		//Also swap internally
-		//buffer = !buffer;
-		//Wait for the swap to be done
-		while (alt_up_pixel_buffer_dma_check_swap_buffers_status(pixel_buffer_dev));
-		usleep(USLEEP_TIME);
-	}*/
-
 	while(1){
 		if ( colorIndicator > 2 ) colorIndicator = 0;
 		alt_up_pixel_buffer_dma_clear_screen(pixel_buffer_dev, buffer);  //Clear the buffer
@@ -75,14 +67,9 @@ int main(void){
 			printf("Buffer swapping failed!\n");
 			return -1;
 		}
-		//Also swap internally
-		//buffer = !buffer;
-		//Wait for the swap to be done
 		while (alt_up_pixel_buffer_dma_check_swap_buffers_status(pixel_buffer_dev));
-
 		//Sleep, softly.
 		usleep(USLEEP_TIME);
 	}
-
 	return 0;
 }
